5c52b5b8ff55ee80cb4e8d396a36ca33da43cf6e
Twizerimana12 <85844144+Twizerimana12@users.noreply.github.com>
2023-03-28 08:53:32 +0200

    last commit

--- database/db.js
+++ database/db.js
@@ -3,12 +3,12 @@ const dotenv =require ("dotenv")
 //const app =require ("app")
 
 dotenv.config()
-// const url=process.env.mongodb
+ const url=process.env.mongodb
 mongoose.set("strictQuery", false);
 
 const db = async () => {
   try {
-    await mongoose.connect('mongodb://0.0.0.0:27017/registration',{ useNewUrlParser: true, useUnifiedTopology: true});
+    await mongoose.connect(url,{ useNewUrlParser: true, useUnifiedTopology: true});
     console.log("Database Connected!");
   } catch (error) {
     console.log("error",error.message);
